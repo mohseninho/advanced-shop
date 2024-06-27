d067ef7616bdad10df509a478ecdf5ae564c3ff6
mohsen <mohsenmsmdev@gmail.com>
2024-06-27 14:29:34 +0330

    project started

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,10 @@
+import style from "./Home.module.css"
+function Home(){
+    return(
+        <div className={style.container}>
+            <h1>asdasdas</h1>
+        </div>
+    );
+}
+
+export default Home;
